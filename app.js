827e0321dcb896a43c998f26f6261b7c52dde6e7
Carlos Ceballo <carlos96cacv@gmail.com>
2021-07-22 14:40:32 -0400

    up

---
+++ app.js
@@ -0,0 +1,12 @@
+const p1 = "Es la frase recurrente en nuestras conversaciones cuando nos damos cuenta de una nueva cosa en común que tenemos y que no teníamos idea, después de unos 3 años de amistad solo nos conocíamos de una manera superficial y en 3 meses pude darme cuenta que eres más rara de lo que creí."
+const p2 = "Eres hermosa (aunque eso no es tan raro), eres buena persona (puntos a favor de tu rareza), eres sincera (esto no lo tiene mucha gente), eres leal (ya esto es muy raro), eres sensible (no me creo tanto eso del hielo), eres misteriosa (no muestras a cualquiera lo que llevas dentro) y además peligrosa (eres como Eau Rouge, la curva más hermosa de la Formula 1 pero también la más peligrosa, cada día que hablo contigo aumenta el riesgo de enamorarme perdidamente de ti), es raro ver una de estas cualidades en una persona, pero tú las tienes todas, eso te hace lo más raro y hermoso que haya visto."
+const p3 = ""
+const p4 = ""
+const p5 = ""
+const p6 = ""
+
+
+
+let text = document.getElementById('text')
+
+text.innerHTML = "<p>"+p1+"</p>"
