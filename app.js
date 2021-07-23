1619454e1fc43dd96bedcc87339e11aba8668679
Carlos Ceballo <carlos96cacv@gmail.com>
2021-07-23 19:34:18 -0400

    update

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const p1 = "Es la frase recurrente en nuestras conversaciones cuando nos damos cuenta de una nueva cosa en común que tenemos y que no teníamos idea, después de unos 3 años de amistad solo nos conocíamos de una manera superficial y en 3 meses pude darme cuenta que eres más rara de lo que creí."
-const p2 = "Eres hermosa (aunque eso no es tan raro), eres buena persona (puntos a favor de tu rareza), eres sincera (esto no lo tiene mucha gente), eres leal (ya esto es muy raro), eres sensible (no me creo tanto eso del hielo), eres misteriosa (no muestras a cualquiera lo que llevas dentro) y además peligrosa (eres como Eau Rouge, la curva más hermosa de la Formula 1 pero también la más peligrosa, cada día que hablo contigo aumenta el riesgo de enamorarme perdidamente de ti), es raro ver una de estas cualidades en una persona, pero tú las tienes todas, eso te hace lo más raro y hermoso que haya visto."
-const p3 = ""
+const p2 = "Eres hermosa (aunque eso no es tan raro), eres buena persona (puntos a favor de tu rareza), eres sincera (esto no lo tiene mucha gente), eres leal (ya esto es muy raro), eres sensible (no me creo tanto eso del hielo), eres misteriosa (no muestras a cualquiera lo que llevas dentro) y..."
+const p3 = "...además peligrosa (eres como Eau Rouge, la curva más hermosa de la Formula 1 pero también la más peligrosa, cada día que hablo contigo aumenta el riesgo de enamorarme perdidamente de ti), es raro ver una de estas cualidades en una persona, pero tú las tienes todas, eso te hace lo más raro y hermoso que haya visto."
 const p4 = ""
 const p5 = ""
 const p6 = ""
-
-
+let i = 0
 
 let text = document.getElementById('text')
 
-text.innerHTML = "<p>"+p1+"</p>"
+if(i == 0){ text.innerHTML = p1 }
+
+function next_page(){
+    console.log(i)
+    i++
+    if(i == 1){ text.innerHTML = p2 }
+    if(i == 2){ text.innerHTML = p3 }
+    if(i == 3){ text.innerHTML = p4 }
+    if(i == 4){ text.innerHTML = p5 }
+    if(i == 5){ text.innerHTML = p6 }
+    
+}
